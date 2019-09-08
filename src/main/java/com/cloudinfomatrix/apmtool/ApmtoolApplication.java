46c1781bbb7a5ee2f33a3ba8d80d7ec3a9d64d6a
Md Akmal Hossain <akmal2080@gmail.com>
2019-09-08 04:12:47 -0400

    first_commit

---
+++ src/main/java/com/cloudinfomatrix/apmtool/ApmtoolApplication.java
@@ -0,0 +1,13 @@
+package com.cloudinfomatrix.apmtool;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApmtoolApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ApmtoolApplication.class, args);
+    }
+
+}
