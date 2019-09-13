0cbf9d6adc87076e58ecf96df217ce4ae01e19a7
Md Akmal Hossain <akmal2080@gmail.com>
2019-09-13 01:05:20 -0400

    Refactor project controller

--- src/main/java/com/cloudinfomatrix/apmtool/domain/Project.java
+++ src/main/java/com/cloudinfomatrix/apmtool/domain/Project.java
@@ -9,6 +9,7 @@ import javax.validation.constraints.Pattern;
 import javax.validation.constraints.Size;
 import java.util.Date;
 
+
 @Entity
 public class Project {
 
--- src/main/java/com/cloudinfomatrix/apmtool/web/ProjectController.java
+++ src/main/java/com/cloudinfomatrix/apmtool/web/ProjectController.java
@@ -1,6 +1,7 @@
 package com.cloudinfomatrix.apmtool.web;
 
 import com.cloudinfomatrix.apmtool.domain.Project;
+import com.cloudinfomatrix.apmtool.services.MapValidationError;
 import com.cloudinfomatrix.apmtool.services.ProjectService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
@@ -25,16 +26,14 @@ public class ProjectController {
     @Autowired
     private ProjectService projectService;
 
+    @Autowired
+    private MapValidationError mapValidationError;
+
     @PostMapping("")
     public ResponseEntity<?> createNewProject(@Valid @RequestBody Project project, BindingResult result){
-        if (result.hasErrors()){
 
-            Map<String,String> errorMap=new HashMap<>();
-            for (FieldError error:result.getFieldErrors()){
-                errorMap.put(error.getField(),error.getDefaultMessage());
-            }
-            return new ResponseEntity<Map<String,String>>( errorMap,HttpStatus.BAD_REQUEST);
-        }
+       ResponseEntity<?> errorMap=mapValidationError.MapValidation(result);
+       if(errorMap!=null) return errorMap;
 
 
         Project project1=projectService.saveOrUpdateProject(project);
